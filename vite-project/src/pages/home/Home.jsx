de54b20e4e904dba1a728e5998d5b5c6d8460c85
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-30 18:10:54 +0530

    Testimonial and Track

--- vite-project/src/pages/home/Home.jsx
+++ vite-project/src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import MyContext from "../../context/data/myContext";
 import HeroSection from "../../coponents/heroSection/HeroSection";
 import Filter from "../../coponents/filter/Filter";
 import ProductCard from "../../coponents/productCard/ProductCard";
+import Testimonial from "../../coponents/testimonial/Testimonial";
+import Track from "../../coponents/track/Track";
 
 function Home() {
   return (
@@ -11,6 +13,8 @@ function Home() {
       <HeroSection />
       <Filter />
       <ProductCard />
+      <Track />
+      <Testimonial />
     </Layout>
   );
 }
