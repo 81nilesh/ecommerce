dd44c923bb1fe37747c12fb917dba28dff1214be
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-30 17:58:26 +0530

    filtter product cart etc...

--- vite-project/src/pages/home/Home.jsx
+++ vite-project/src/pages/home/Home.jsx
@@ -1,9 +1,18 @@
 import { useContext } from "react";
 import Layout from "../../coponents/layout/Layout";
 import MyContext from "../../context/data/myContext";
+import HeroSection from "../../coponents/heroSection/HeroSection";
+import Filter from "../../coponents/filter/Filter";
+import ProductCard from "../../coponents/productCard/ProductCard";
 
 function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <HeroSection />
+      <Filter />
+      <ProductCard />
+    </Layout>
+  );
 }
 
 export default Home;
