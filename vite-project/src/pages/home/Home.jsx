631516e7159421efa8b3df22e579d73249e69498
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-30 17:41:44 +0530

    first commit

---
+++ vite-project/src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import Layout from "../../coponents/layout/Layout";
+import MyContext from "../../context/data/myContext";
+
+function Home() {
+  return <Layout>Home</Layout>;
+}
+
+export default Home;
