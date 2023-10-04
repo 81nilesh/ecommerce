ddadf0b4ad2ca671c69db20ddc56924751a2588e
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-10-04 08:14:35 +0530

    commit

--- vite-project/src/pages/admin/dashboard/DashboardTab.jsx
+++ vite-project/src/pages/admin/dashboard/DashboardTab.jsx
@@ -127,36 +127,32 @@ function DashboardTab() {
                             <td
                               className="px-6 py-4 text-black "
                               style={{ color: mode === "dark" ? "white" : "" }}>
-                              1.
+                              {index + 1}.
                             </td>
                             <th
                               scope="row"
                               className="px-6 py-4 font-medium text-black whitespace-nowrap">
-                              <img
-                                className="w-16"
-                                src="https://dummyimage.com/720x400"
-                                alt="img"
-                              />
+                              <img className="w-16" src={imageUrl} alt="img" />
                             </th>
                             <td
                               className="px-6 py-4 text-black "
                               style={{ color: mode === "dark" ? "white" : "" }}>
-                              Title
+                              {title}
                             </td>
                             <td
                               className="px-6 py-4 text-black "
                               style={{ color: mode === "dark" ? "white" : "" }}>
-                              ₹100
+                              ₹{price}
                             </td>
                             <td
                               className="px-6 py-4 text-black "
                               style={{ color: mode === "dark" ? "white" : "" }}>
-                              pots
+                              {category}
                             </td>
                             <td
                               className="px-6 py-4 text-black "
                               style={{ color: mode === "dark" ? "white" : "" }}>
-                              12 Aug 2019
+                              {date}
                             </td>
                             <td className="px-6 py-4">
                               <div className=" flex gap-2">
@@ -180,21 +176,23 @@ function DashboardTab() {
                                       />
                                     </svg>
                                   </div>
-                                  <div>
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      fill="none"
-                                      viewBox="0 0 24 24"
-                                      strokeWidth={1.5}
-                                      stroke="currentColor"
-                                      className="w-6 h-6">
-                                      <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                                      />
-                                    </svg>
-                                  </div>
+                                  <Link to={"/updateproduct"}>
+                                    <div>
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="w-6 h-6">
+                                        <path
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                                        />
+                                      </svg>
+                                    </div>
+                                  </Link>
                                 </div>
                               </div>
                             </td>
